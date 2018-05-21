75147d30ee8d0210e587f9ec72df4a8a4cbf25ae
jajamichael321123321 <38138508+jajamichael321123321@users.noreply.github.com>
2018-05-21 12:25:47 -0400

    Update ttt.cpp

--- ttt.cpp
+++ ttt.cpp
@@ -1,18 +1,67 @@
 #include <iostream>
+#include <string>
 
-void printBoard(int board[3][3]);
+	using namespace std;
+
+void printBoard(int board[3][3], bool player1Turn,string name);
+int checkWin(int board[3][3]);
+int sumRow(int row[3]);
+int sumCol(int board[3][3], int column);
+std::string name1;
+std::string name2;
 
 int main() {
+
+	
+	std::cout << "Enter Player 1 Username : ";
+	std::cin >> name1;
+	std::cout << "Enter Player 2 Username : ";
+	std::cin >> name2;
+
+
+	
+	
+	
+	bool nameTurn = true;
+	
 	int board[3][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+	int row, col, result = 0;
 	
-	printBoard(board);
-	
+	while(checkWin(board) == 0) {
+		printBoard(board, nameTurn,name1);
+		std::cout << "Enter a row and a column: ";
+		std::cin >> row >> col;
+		
+		while(board[row-1][col-1] != 0) {
+			std::cout <<"Enter a valid, empty row and column choice: ";
+			std::cin >> row >> col;
+		}
+		
+		board[row-1][col-1] = nameTurn ? 1 : -1;
+		//Switch players
+		nameTurn = !nameTurn;
+	}
+	printBoard(board, nameTurn,name1);
 	return 0;
 }
 
-void printBoard(int board[3][3]) {
+void printBoard(int board[3][3], bool nameTurn,string name2) {
+	if(nameTurn) {
+		std::cout << name1 <<  name1 << std::endl;
+	} else {
+		std::cout << name2 << name2 << std::endl;
+	}
+
+	std::cout << std::endl << "   ";
+	//Column headers
 	for(int i = 0; i < 3; i++) {
+		std::cout << " " << i+1 << "  ";
+	}
+	std::cout << std::endl;
+	for(int i = 0; i < 3; i++) {
+		std:: cout << " " << i+1 << " "; //Row headers
 		for(int j = 0; j < 3; j++) {
+			//Board uses 0 for unused, 1 for X and -1 for O
 			switch(board[i][j]) {
 				case 0:
 					std::cout << "   ";
@@ -24,7 +73,49 @@ void printBoard(int board[3][3]) {
 					std::cout << " O ";
 					break;
 			}
+			if(j != 2) {
+				std::cout << "|"; //Lines between the columns
+			}
 		}
 		std::cout << std::endl;
+		if(i != 2) {
+			//Print lines between rows
+			std::cout << "   ---+---+---" << std::endl;
+		}
 	}
+}
+
+int checkWin(int board[3][3]) {
+	int result = 0;
+	
+	
+	//Check rows and cols for wins
+	for(int i = 0; i < 3; i++) {
+		int rowSum = sumRow(board[i]), colSum = sumCol(board, i);
+		if(rowSum > 2 || colSum > 2) {
+			result = 1;
+		} else if(rowSum < -2 || colSum < -2) {
+			result = -1;
+		}
+	}
+	
+	//Check diagonals for wins
+	int diagTop = board[0][0] + board[1][1] + board[2][2],
+		diagBot = board[2][0] + board[1][1] + board[0][2];
+			
+	if(diagTop > 2 || diagBot > 2) {
+		result = 1;
+	} else if(diagTop < -2 || diagBot < -2) {
+		result = -1;
+	}
+	
+	return result;
+}
+
+int sumRow(int row[3]) {
+	return row[0] + row[1] + row[2];
+}
+
+int sumCol(int board[3][3], int column) {
+	return board[0][column] + board[1][column] + board[2][column];
 }
